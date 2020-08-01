8e9ef03d9db8f072507d33e0c3984a6ad29fce8f
mekwall <marcus.ekwall@gmail.com>
2020-08-01 18:24:33 +0200

    chore(lint-staged): remove git add

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,11 +1,6 @@
 /* eslint-env commonjs */
 module.exports = {
-  "*.{ts,tsx}": [
-    "eslint --fix",
-    "prettier --parser typescript --write",
-    "git add",
-    // "jest --bail --findRelatedTests",
-  ],
-  "*.{js,jsx}": ["eslint --fix", "prettier --write", "git add"],
-  "*.{md,yaml,json}": ["prettier --write", "git add"],
+  "*.{ts,tsx}": ["eslint --fix", "prettier --parser typescript --write"],
+  "*.{js,jsx}": ["eslint --fix", "prettier --write"],
+  "*.{md,yaml,json}": ["prettier --write"],
 };
