95c42b69a7c6f475050fc0aae04c73b1d6cf40ec
mekwall <marcus.ekwall@gmail.com>
2020-08-01 16:45:48 +0200

    chore(commitlint): allow scope to be anything

--- commitlint.config.js
+++ commitlint.config.js
@@ -7,7 +7,6 @@ module.exports = {
     "footer-leading-blank": [1, "always"],
     "footer-max-line-length": [2, "always", 100],
     "header-max-length": [2, "always", 100],
-    "scope-case": [2, "always", ["lower-case", "pascal-case", "upper-case"]],
     "subject-case": [
       2,
       "never",
